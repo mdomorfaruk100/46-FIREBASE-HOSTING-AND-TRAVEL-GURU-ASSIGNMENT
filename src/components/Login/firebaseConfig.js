c2dac4596482c44f818640da4eb98772b635dbe3
mdomorfaruk100 <mdomorfarukdeveloper988@gmail.com>
2025-12-19 23:26:03 +0600

    Update firebaseConfig.js

--- src/components/Login/firebaseConfig.js
+++ src/components/Login/firebaseConfig.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-     apiKey: "AIzaSyC9X-JK9GqeIoWUw3nRJakW6CmqApPaaS4",
-     authDomain: "ema-john-simple-e2817.firebaseapp.com",
-     projectId: "ema-john-simple-e2817",
-     storageBucket: "ema-john-simple-e2817.firebasestorage.app",
-     messagingSenderId: "507690471701",
-     appId: "1:507690471701:web:ac9ad7617296c68d41191c",
-     measurementId: "G-Z2QML2R4YS"
+  
 };
 
 export default firebaseConfig;
